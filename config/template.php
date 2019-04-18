c04f55c9f6f5a67eb03b1670f535fd9daaa652d9
zhaoyuqi <zhaoyuqi@xindong.com>
2019-04-18 15:30:46 +0800

    修改模板后缀为twig

--- config/template.php
+++ config/template.php
@@ -21,7 +21,7 @@ return [
     // 模板路径
     'view_path'    => '',
     // 模板后缀
-    'view_suffix'  => 'html',
+    'view_suffix'  => 'twig',
     // 模板文件名分隔符
     'view_depr'    => DIRECTORY_SEPARATOR,
     // 模板引擎普通标签开始标记
