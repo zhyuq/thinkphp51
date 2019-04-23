18da849ac4d3733691c474d2cbd1fadb3d786056
zhaoyuqi <zhaoyuqi@xindong.com>
2019-04-23 20:02:10 +0800

    把传到模板的参数格式改成：[key->value]  ==> [key -> array(key: value)]

--- application/controller/Asset.php
+++ application/controller/Asset.php
@@ -24,15 +24,14 @@ class Asset extends Controller
             $path_arr = explode("/", $value);
             $path_level = count($path_arr);
             $path_tail = substr($value, strlen("/Users/staff/坚果云-典藏版/"));
-            if ($path_level == 5) {
-//                Log::debug($path_tail);
-                $assign_file_list[] = " -- " . $path_tail;
-            } elseif ($path_level >= 6) {
-                $baseName = basename($path_tail);
-                $targetName = sprintf("%s%s -- %s", str_repeat('&nbsp;', $path_level-5), str_repeat("|", $path_level-5), $baseName);
+            $baseName = basename($path_tail);
+            $targetName = sprintf("%s%s -- %s", str_repeat('&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;', $path_level-5), str_repeat("|", $path_level-5), $baseName);
 
-                $assign_file_list[] = $targetName;
-            }
+            $assign_file_value = array();
+            $assign_file_value["baseName"] = $baseName;
+            $assign_file_value["targetName"] = $targetName;
+            $assign_file_list[] = $assign_file_value;
+
         }
         Log::debug($assign_file_list);
         $this->assign("files", $assign_file_list);
