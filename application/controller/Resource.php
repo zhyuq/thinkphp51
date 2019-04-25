3d251e7198abda44e2add6cb1f78ef206e9b42b5
zhyq <1064639871@qq.com>
2019-04-25 20:36:46 +0800

    定义全局常量，优化逻辑

--- application/controller/Resource.php
+++ application/controller/Resource.php
@@ -3,34 +3,52 @@
 
 namespace app\controller;
 
+use think\App;
 use think\Controller;
 use think\facade\Log;
 use think\Request;
 
 class Resource extends Controller
 {
+    protected $originPath;
+    protected $resPath;
+
+    public function __construct(App $app = null)
+    {
+        parent::__construct($app);
+
+        $this->originPath = $this->request->server("HOME") . "/" . ORIGINAL_PATH;
+    }
+
     public function index()
     {
 
         $file_list = array();
-        \zyq\File::visit("/Users/staff/坚果云-典藏版", true, function ($file) use (&$file_list) {
-            if (is_dir($file) && (strpos($file, "新UI界面") !== false)) {
-                $file_list[] = $file;
-            }
+        \zyq\File::visit($this->originPath, true, function ($file) use (&$file_list) {
+            if (!is_dir($file))
+                return;
 
+            if (preg_match('/.*(策划档|多语言|启动图和icon|SDK).*/i', $file))
+                return;
+
+            $file_list[] = $file;
         });
 
         $assign_file_list = array();
         foreach ($file_list as $key => $value) {
-            $path_arr = explode("/", $value);
-            $path_level = count($path_arr);
-            $path_tail = substr($value, strlen("/Users/staff/坚果云-典藏版/"));
+            $config = is_file("{$value}/.admin_config") ? json_decode(file_get_contents("{$value}/.admin_config"), true) : null;
+            $path_level = substr_count($value, "/");
+            $path_tail = substr($value, strlen($this->originPath)+1);
             $baseName = basename($path_tail);
-            $targetName = sprintf("%s%s -- %s", str_repeat('&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;', $path_level-5), str_repeat("|", $path_level-5), $baseName);
+            $targetName = sprintf("%s%s -- %s", str_repeat('&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;', $path_level-4), str_repeat("|", $path_level-4), $baseName);
 
             $assign_file_value = array();
             $assign_file_value["filePath"] = $path_tail;
             $assign_file_value["targetName"] = $targetName;
+            $assign_file_value["export_name"] = $config["export_name"];
+            $assign_file_value["change_mode"] = $config["change_mode"];
+            $assign_file_value["type"] = $config["type"];
+            $assign_file_value["language"] = $config["language"];
             $assign_file_list[] = $assign_file_value;
 
         }
@@ -39,7 +57,7 @@ class Resource extends Controller
         return $this->fetch();
     }
 
-    public function build(Request $request)
+    public function build()
     {
         return "resource build";
     }
